6f1345cdbc7cdbcc2f6f043e2e16ac50ecdca26e
Vishal Kumar <vishal.kumar@christuniversity.in>
2021-09-20 06:54:47 +0530

    added useReducer and deleted popup

--- src/components/useReducer/useReducer.jsx
+++ src/components/useReducer/useReducer.jsx
@@ -1,9 +1,24 @@
 import React, {useReducer} from 'react';
 
+let initialState = 0;
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCR':
+      return state + 1;
+    case 'DECR':
+      return state - 1;
+    default:
+      return state;
+  }
+}
 const UseReducer = () => {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <h1>useReducer</h1>
+      <h1>useReducer Hook</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch({type: 'INCR'})}>Increment</button>
+      <button onClick={() => dispatch({type: 'DECR'})}>Decrement</button>
     </div>
   );
 };
